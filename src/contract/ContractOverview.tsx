2d27d6ee98f1d4dbaee732cb590ed40c8172162a
Gerben Jongerius <g.jongerius@jong-soft.com>
2023-05-26 16:13:16 +0200

    Fix tabs on the contract page.

---
+++ src/contract/ContractOverview.tsx
@@ -0,0 +1,36 @@
+import {FC, useEffect, useState} from "react";
+import {ContractRepository} from "../core/RestAPI";
+import {BreadCrumbItem, BreadCrumbs, Buttons, Layout} from "../core";
+import {ContractTable} from "./ContractTable";
+import {mdiClockTimeTwoOutline, mdiHistory} from "@mdi/js";
+
+export const ContractOverview: FC<void> = () => {
+    const [contracts, setContracts] = useState()
+    const [tabSection, setTabSection] = useState('active')
+
+    useEffect(() => {
+        ContractRepository.list()
+            .then(setContracts)
+    }, [])
+
+    return <>
+        <div className='ContractOverview'>
+            <BreadCrumbs>
+                <BreadCrumbItem label='page.nav.finances' />
+                <BreadCrumbItem label='page.nav.budget.contracts' />
+            </BreadCrumbs>
+
+            <Layout.Card title='page.budget.contracts.title'>
+                <Layout.Tabs onChange={setTabSection}
+                             activeTab={tabSection}
+                             buttons={[
+                                 {id: 'active', title: 'page.budget.contracts.active', icon: mdiClockTimeTwoOutline},
+                                 {id: 'terminated', title: 'page.budget.contracts.inactive', icon: mdiHistory}]}>
+
+                    {!contracts && <Layout.Loading />}
+                    {contracts && <ContractTable contracts={contracts[tabSection]} />}
+                </Layout.Tabs>
+            </Layout.Card>
+        </div>
+    </>
+}
--- src/core/layout/index.js
+++ src/core/layout/index.js
@@ -1,9 +1,11 @@
 import Grid from "./Grid"
 import Card from "./Card"
 import Loading from "./Loading"
+import Tabs from "./Tabs"
 
 export {
     Grid,
     Card,
-    Loading
+    Loading,
+    Tabs
 }
