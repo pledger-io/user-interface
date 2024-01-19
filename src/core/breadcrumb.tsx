53936ad86796b8078eb8cba8b1f1eed639616697
Gerben Jongerius <g.jongerius@jong-soft.com>
2024-01-19 11:45:25 +0100

    FIN-341 resolve styling issue in breadcrumb item with link.

--- src/core/breadcrumb.tsx
+++ src/core/breadcrumb.tsx
@@ -24,7 +24,7 @@ export const BreadCrumbs = ({ children }: PropsWithChildren) => {
 export const BreadCrumbItem = ({ message, label, href }: { message?: string, label?: string, href?: string }) => {
     let text = label ? <Translations.Translation label={label}/> : message;
     if (href) {
-        text = <Link to={ href } className="flex-1 max-w-fit before:content-['\\'] before:mr-1 [&:nth-child(2)]:before:content-['']">{ text }</Link>
+        text = <Link to={ href } className="flex-1 max-w-fit">{ text }</Link>
     }
 
     return <li className="flex-1 max-w-fit before:content-['\\']
