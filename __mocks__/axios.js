3189f6d5c4a39052cd21155c7b605beab306435f
Gerben Jongerius <atlassian@jong-soft.com>
2024-02-29 15:44:00 +0000

    axios.js edited online with Bitbucket

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -2,6 +2,7 @@
 const axiosMock = {
     get: jest.fn(() => Promise.resolve({data: {}})),
     post: jest.fn(() => Promise.resolve({data: {}})),
+    create: _ => axiosMock
 }
 
 export default axiosMock
