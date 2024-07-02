8357ed2f2b943576ec206ab5bc27c52f44fc9a53
Gerben Jongerius <g.jongerius@jong-soft.com>
2024-07-02 17:47:30 +0200

    Fix the grid test.

--- src/components/layout/grid.component.spec.tsx
+++ src/components/layout/grid.component.spec.tsx
@@ -1,10 +1,10 @@
-import Grid from "../../components/layout/Grid";
+import Grid from "./grid.component"
 import { render } from "@testing-library/react";
 
 describe(Grid, () => {
 
     it("A column grid should have template columns", () => {
-        const { container } = render(<Grid type='column' minWidth={'200px'}><div/></Grid>)
+        const { container } = render(<Grid type='column' minWidth={'200px'}><div/><div/></Grid>)
 
         const grid = container.querySelector('.grid')
         expect(grid).toHaveStyle({ gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))' })
