de6d2193610ed5b433b2076087e46b888df5afc4
Gerben Jongerius <gjong@users.noreply.github.com>
2024-08-16 14:31:34 +0200

    Add custom UUID function

--- src/core/index.js
+++ src/core/index.js
@@ -86,18 +86,13 @@ const Resolver = {
         }
     },
     uuid: () => {
-        return window.crypto !== undefined ? window.crypto.randomUUID() : '' + Date.now()
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+            .replace(/[xy]/g, function (c) {
+                const r = Math.random() * 16 | 0, 
+                    v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
     }
-}
-
-// add the randomUUID option to crypto
-window.crypto.randomUUID = function () {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-        .replace(/[xy]/g, function (c) {
-            const r = Math.random() * 16 | 0, 
-                v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
 }
 
 export {
