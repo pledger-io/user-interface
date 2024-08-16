4e802f19ddbbe964fa79433a550367d53acb7f09
Gerben Jongerius <gjong@users.noreply.github.com>
2024-08-16 14:26:11 +0200

    Update index.js

--- src/core/index.js
+++ src/core/index.js
@@ -90,6 +90,16 @@ const Resolver = {
     }
 }
 
+// add the randomUUID option to crypto
+window.crypto.randomUUID = function () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+        .replace(/[xy]/g, function (c) {
+            const r = Math.random() * 16 | 0, 
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+}
+
 export {
     Attachment,
     When,
