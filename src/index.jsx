6b85885494600c79142312bf5ff797705acedbf5
Gerben Jongerius <g.jongerius@jong-soft.com>
2025-05-27 19:37:36 +0200

    Update theme handling and introduce static theme copying
    Refactored theme handling logic to dynamically load themes with `PrimeReactContext` in React and removed unused theme imports. Integrated `vite-plugin-static-copy` to copy Primereact theme files to the project assets during the build.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import Pledger from './pledger-io';
 import { createRoot } from "react-dom/client";
 import PrimeLocale from "./config/prime-locale";
 
 import { Card } from "primereact/card";
 import { Button } from "primereact/button";
+import {Themes} from "./context/theme-context.js";
+import {PrimeReactContext, PrimeReactProvider} from "primereact/api";
+
+const themeLink = document.getElementById('theme-link')
+document.head.removeChild(themeLink);
+document.head.appendChild(themeLink);
 
 PrimeLocale()
   .then(() => {
@@ -21,32 +27,44 @@ PrimeLocale()
     createRoot(document.getElementById('root'))
       .render((
         <React.StrictMode>
-          <div className="flex items-center justify-center h-screen bg-gray-100">
-            <Card
-              className="w-1/2 mx-auto h-auto bg-teal-100 border-gray-200 rounded-lg"
-              header={
-                <div className="text-center pt-3">
-                  <i className="pi pi-exclamation-triangle" style={{ fontSize: '3em', color: '#dc3545' }}></i>
-                  <h1 className="text-2xl font-extrabold">Pledger.io failed to load</h1>
-                </div>
-              }>
-              <div className="text-center">
-                <p className="mb-3">
-                  Oops! An unexpected error occurred while loading the application.
-                  Please try again later.
-                </p>
-                <Button
-                  label="Reload"
-                  icon="pi pi-refresh"
-                  className="p-button-raised p-button-rounded"
-                  onClick={() => window.location.reload()}
-                />
-              </div>
-              <p className="text-sm text-center mt-3">
-                If the problem persists, please contact support.
-              </p>
-            </Card>
-          </div>
+          <PrimeReactProvider>
+              <LoadingFailed />
+          </PrimeReactProvider>
         </React.StrictMode>
       ));
   });
+
+const LoadingFailed = ({}) => {
+  const { changeTheme } = useContext(PrimeReactContext);
+
+  useEffect(() => {
+    changeTheme(Themes.light.stylesheet, Themes.light.stylesheet, 'theme-link')
+  }, []);
+
+  return <div className="flex items-center justify-center h-screen bg-gray-100">
+    <Card
+      className="w-1/2 mx-auto h-auto bg-teal-100 border-gray-200 rounded-lg"
+      header={
+        <div className="text-center pt-3">
+          <i className="pi pi-exclamation-triangle" style={{ fontSize: '3em', color: '#dc3545' }}></i>
+          <h1 className="text-2xl font-extrabold">Pledger.io failed to load</h1>
+        </div>
+      }>
+      <div className="text-center">
+        <p className="mb-3">
+          Oops! An unexpected error occurred while loading the application.
+          Please try again later.
+        </p>
+        <Button
+          label="Reload"
+          icon="pi pi-refresh"
+          className="p-button-raised p-button-rounded"
+          onClick={() => window.location.reload()}
+        />
+      </div>
+      <p className="text-sm text-center mt-3">
+        If the problem persists, please contact support.
+      </p>
+    </Card>
+  </div>
+}
