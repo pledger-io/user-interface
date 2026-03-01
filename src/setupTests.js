e61cd6d3064aa9c54280569d31bfa36549cb9433
Gerben Jongerius <g.jongerius@jong-soft.com>
2026-03-01 18:17:01 +0100

    Fix localStorage in tests.

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,7 +5,16 @@
 import '@testing-library/jest-dom';
 import { TextEncoder } from 'util';
 import {mockedAxios} from "../__mocks__/axios.js";
+import {vi} from "vitest";
 
 global.TextEncoder = TextEncoder;
 
 mockedAxios.get()
+
+let localStore = new Map()
+vi.stubGlobal('localStorage', {
+    getItem: vi.fn().mockImplementation(prop => localStore.get(prop)),
+    setItem: vi.fn().mockImplementation((prop, value) => localStore.set(prop, value)),
+    removeItem: vi.fn(),
+    clear: vi.fn()
+});
